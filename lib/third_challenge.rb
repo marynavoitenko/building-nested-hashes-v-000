7f8d2b9ede93a2af0a8e315925081a30bb11ab3b
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-31 01:15:01 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,16 +4,16 @@ def third_challenge
         :patriarch => {
           :name => "Lord Montague",
           :age => "53"
-        }
+        },
         :matriarch => {
           :name => "Lady Montague",
           :age => "54"
-        }
+        },
         :hero => {
           :name => "Romeo",
           :age => "15",
           :status => "alive"
-        }
+        },
         :hero_friends => [
           {
             :name => "Benvolio",
